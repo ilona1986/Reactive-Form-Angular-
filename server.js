65191173436eb6d5471e1200aa79c3a592c75b86
Ilona <Ilona_b9ka@mail.ru>
2021-11-10 11:07:55 +0300

    add prod for deploy

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 // serve static files....
-app.use(express.static(`${__dirname}/dist/app-Reactive-Forms`));
+app.use(express.static(`${__dirname}/dist/app-Template-Driven-Forms`));
 // send all requests to index.html
 app.get('/*', (req, res) => {
   res.sendFile(path.join(`${__dirname}/dist/angular-github-deploy/index.html`));
